ee85e07a8661f027a38376b14161fbf3666ba179
ADE <sikirusulaimon2002@gmail.com>
2025-03-15 10:49:55 +0100

    test

--- number.py
+++ number.py
@@ -1,2 +1,7 @@
 num1 = input("enter number one: ");
 num1 = int(num1);
+num2 = input("enter number two: ");
+num2 = int(num2);
+num3 = input("enter number three: ");
+num3 = int(num3);
+print("number one {0} number two {1} number three {2}".format(num1, num2, num3)");
