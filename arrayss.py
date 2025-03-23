11b1b64a5ea0d354bc9b52c20a1e4d6a1f1760ec
ADE <sikirusulaimon2002@gmail.com>
2025-03-23 11:42:02 +0100

    practice

---
+++ arrayss.py
@@ -0,0 +1,26 @@
+import math
+arr=[]
+n = input("array length: ")
+n = int(n)
+for i in range(n):
+    val = input('enter value: %d' %i)
+    val = int(val)
+    arr.append(val)
+print(arr)
+suma = 0
+for i in range(n):
+    suma = suma + arr[i]
+    print(suma, end= ',')
+print()
+ave = suma/n
+var = 0
+for i in range(n):
+    var = var +((arr[i]-ave)*(arr[i]-ave))
+print('variation is ' , var)
+sd = math.sqrt(var)
+print('standard deviation', sd)
+sd = '{0:.03f}'.format(sd)
+ave = '{0:.02f}'.format(ave)
+print("average in 2 decimal places is ", ave)
+print("standard deviation in 3 decimal places is ", sd)
+print("addition ", suma)
